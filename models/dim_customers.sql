10859c16572f6728298a0e6d502c96330f766ad4
michael.heinecke@ingka.ikea.com <michael.heinecke@ingka.ikea.com>
2022-12-27 12:39:34 +0000

    Change BQ source project

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,7 +1,11 @@
+{{ config(materialized="table") }}
+
 with
     customers as (
 
-        select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
+        select id as customer_id, first_name, last_name
+
+        from `dbt-tutorial`.jaffle_shop.customers
 
     ),
 
@@ -9,7 +13,7 @@ with
 
         select id as order_id, user_id as customer_id, order_date, status
 
-        from raw.jaffle_shop.orders
+        from `dbt-tutorial`.jaffle_shop.orders
 
     ),
 
@@ -27,7 +31,6 @@ with
         group by 1
 
     ),
-
 
     final as (
 
